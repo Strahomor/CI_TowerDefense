662729f8440b5b64cf36dff67d93ebb73a17d132
Strahomor <viktorvrtar1945@gmail.com>
2023-02-05 14:31:35 +0100

    Camera fix :)

--- TowerDefense_Project/Assets/C# Scripts/CameraController.cs
+++ TowerDefense_Project/Assets/C# Scripts/CameraController.cs
@@ -14,12 +14,21 @@ public class CameraController : MonoBehaviour
     private float cam_z_move_min = -150f;
     private float cam_z_move_max = 350f;
 
+    private float cam_rotate_x_min;
+    private float cam_rotate_x_max;
+    private float cam_rotate_y_min;
+    private float cam_rotate_y_max;
+
     public float sensitivity = 5.0f;
 
     public float hInput;
     public float vInput;
 
     public float VerticalMouse;
+    public float HorizontalMouse;
+
+    private float prevRotation;
+    private Vector3 startingRotation;
 
     public float sped = 150.0f;
 
@@ -27,15 +36,19 @@ public class CameraController : MonoBehaviour
     private Vector3 direction2;
 
     public GameObject Playa;
+    public GameObject Pivot;
+
+    public bool lockedy;
+    public bool lockedx;
 
     //350, -150z
     //585, -410z
     // Start is called before the first frame update
     void Start()
     {
-       
+        startingRotation = new Vector3(transform.rotation.x, transform.rotation.y, transform.rotation.z);
     }
-
+        
     // Update is called once per frame
     void Update()
     {
@@ -50,11 +63,20 @@ public class CameraController : MonoBehaviour
         if (((Playa.transform.position.x < cam_x_move_min) && (Playa.transform.position.x > cam_x_min)) || (((Playa.transform.position.x > cam_x_move_max)) && (Playa.transform.position.x < cam_x_max))) { transform.Translate(Vector3.right * hInput * Time.deltaTime * sped); }
         if (((Playa.transform.position.z < cam_z_move_min) && (Playa.transform.position.z > cam_z_min)) || ((Playa.transform.position.z > cam_z_move_max) && (Playa.transform.position.z < cam_z_max))) { transform.Translate(Vector3.up * vInput * Time.deltaTime * sped); }
 
-        if (Input.GetMouseButton(1))
-        {
+        if (Input.GetMouseButton(1)) {
+            //Debug.Log(startingRotation);
+            prevRotation = transform.rotation.z;
             VerticalMouse = Input.GetAxis("Mouse Y");
-            transform.RotateAround(Playa.transform.position, Playa.transform.right, VerticalMouse * sensitivity);
+            HorizontalMouse = Input.GetAxis("Mouse X");
+
+            //if ((Input.GetAxis("Mouse X") != 0) && (lockedy == false)) { }
+
+            if (Input.GetAxis("Mouse X") != 0) { transform.RotateAround(Pivot.transform.position, Pivot.transform.up, HorizontalMouse * sensitivity);  }
+            if (Input.GetAxis("Mouse Y")!=0) { transform.RotateAround(Pivot.transform.position, Pivot.transform.right, -VerticalMouse * sensitivity);  }
         }
+        if (transform.rotation.z != startingRotation.z) {
+            transform.LookAt(Pivot.transform);
+            }
 
     }
 }
